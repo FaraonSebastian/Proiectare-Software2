4603d88829a4a1da5df782553870ce15ec7d1c0b
FaraonSebastian <faraonsebastian10@yahoo.com>
2025-04-01 16:11:24 +0300

    p1 a b c

---
+++ src/Appplication.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Appplication {
+    public static void main(String[] args) {
+        try {
+            printSmallTextFile("in.txt","out.txt");
+            System.out.println("\n\n");
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static void printSmallTextFile(String inputfile,String outputfile) throws IOException {
+        System.out.println("Using Filesc.readAllLines:");
+
+        Path inputPath=Paths.get(inputfile);
+        Path outputPath=Paths.get(outputfile);
+
+        List<String>allLinesInMemory = Files.readAllLines(inputPath);
+        StringBuilder modifiedContent = new StringBuilder();
+
+
+        for(String line: allLinesInMemory){
+            System.out.println(line+"\n");
+
+            String modifiedLine=line.replaceAll("\\.", "\n ");
+            System.out.println(modifiedLine);
+
+            modifiedContent.append(modifiedLine).append("\n");
+        }
+        Files.write(outputPath, modifiedContent.toString().getBytes());
+    }
+
+}
